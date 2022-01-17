afc9ae9e5c61ab728a0b944b48ff5c71d572e0b9
James Krieger <jamesmkrieger@gmail.com>
2022-01-17 11:47:18 +0000

    removed hello world

--- prody2/protocols/__init__.py
+++ prody2/protocols/__init__.py
@@ -3,5 +3,4 @@
 # Module to declare protocols
 # Find documentation here: https://scipion-em.github.io/docs/docs/developer/creating-a-protocol
 # **************************************************************************
-from .protocol_anm import prodyANM
-from .protocol_hello_world import prodyPrefixHelloWorld
+from .protocol_anm import ProDyANM
