ba5700c1ff3a8254a6dafdd173d4a023018a8d11
James Krieger <jamesmkrieger@gmail.com>
2022-02-23 17:53:31 +0000

    improved viewer

--- prody2/viewers/viewer_compare.py
+++ prody2/viewers/viewer_compare.py
@@ -39,7 +39,9 @@ from prody2.protocols.protocol_compare import NMA_METRIC_OVERLAP
 
 import numpy as np
 import os
+
 import prody
+from prody.utilities.drawtools import IndexFormatter
 
 class ProDyComparisonsViewer(ProtocolViewer):
     """ Visualization of results from the ProDy mode comparison protocol.    
@@ -52,38 +54,63 @@ class ProDyComparisonsViewer(ProtocolViewer):
         metric = self.protocol.metric.get()
         diag = self.protocol.diag.get()
 
+        self.modes1 = self.protocol.modes1.get()
+        self.modes2 = self.protocol.modes2.get()
+        many_modes = len(self.modes1) > 1 and len(self.modes2) > 1
+
+        have_matrix = (many_modes and metric==NMA_METRIC_OVERLAP and diag==False)
+
         form.addSection(label='Visualization')
   
-        form.addParam('displayWhole', LabelParam,
-                      label='Display whole output matrix?', important=True,
-                      help='Truly 2D matrices are shown as heatmaps. '
-                           'Diagonal or cumulative values are shown as bars.')
-        
-        group = form.addGroup('Single mode', 
-                              condition=(metric==NMA_METRIC_OVERLAP and diag==False))      
-        group.addParam('modeNumber', IntParam, default=7,
+        form.addParam('displayMatrix', LabelParam,
+                      condition=have_matrix == True,
+                      label='Display matrix?',
+                      help='Overlap matrices are shown as heatmaps.')
+
+        form.addParam('displayBarsMain', LabelParam,
+                      condition=have_matrix == False,
+                      label='Display bar graphs?',
+                      help='Values are shown as bars.')
+
+        group = form.addGroup('Single mode from set 1',
+                              condition=have_matrix == True)
+        group.addParam('modeNumSet1', IntParam, default=7,
                        label='Mode number')
-        group.addParam('displayBars', LabelParam, default=False,
-                       label='Display matrix row?',
+        group.addParam('displayBarsSet1', LabelParam, default=False,
+                       label='Display bar graphs?',
                        help='Matrix rows are shown as bars.')
-        group.addParam('displayCumulOverlap', BooleanParam, default=False, 
+        group.addParam('cumulOverlapSet1', BooleanParam, default=False,
                        condition=metric==NMA_METRIC_OVERLAP,
                        label='Display cumulative overlap?',
                        help='Matrix rows are shown as bars.')
 
+        group = form.addGroup('Single mode from set 2',
+                              condition=have_matrix == True)
+        group.addParam('modeNumSet2', IntParam, default=7,
+                       label='Mode number')
+        group.addParam('displayBarsSet2', LabelParam, default=False,
+                       label='Display bar graphs?',
+                       help='Matrix rows are shown as bars.')
+        group.addParam('cumulOverlapSet2', BooleanParam, default=False,
+                       condition=metric==NMA_METRIC_OVERLAP,
+                       label='Display cumulative overlap?',
+                       help='Matrix rows are shown as bars.')
+
+        form.addParam('cumulOverlapMain', BooleanParam, default=False,
+                      condition=(metric==NMA_METRIC_OVERLAP and have_matrix==False),
+                      label='Display cumulative overlap?',
+                      help='Matrix rows are shown as bars.')
+
         form.addParam('abs', BooleanParam, default=True,
-                       label='Show absolute values?',
-                       help='Otherwise signed values are shown.', 
-                       condition=metric==NMA_METRIC_OVERLAP)
+                      label='Show absolute values?',
+                      help='Otherwise signed values are shown.',
+                      condition=metric == NMA_METRIC_OVERLAP)
 
     def _getVisualizeDict(self):
-        matrix = prody.parseArray(self.protocol.matrixFile.getFileName())
-
-        if matrix.ndim == 2:
-            return {'displayWhole': self._viewMatrix,
-                    'displayBars': self._viewSingleMode} 
-        else:
-            return {'displayWhole': self._viewSingleMode}             
+        return {'displayMatrix': self._viewMatrix,
+                'displayBarsMain': self._viewSingleMode,
+                'displayBarsSet1': self._viewSingleMode,
+                'displayBarsSet2': self._viewSingleMode}            
 
     def _viewMatrix(self, paramName):
         """ visualisation for 2D mode comparisons""" 
@@ -98,20 +125,29 @@ class ProDyComparisonsViewer(ProtocolViewer):
             vmax = 1
 
         plotter = EmPlotter()
-        plotter.createSubPlot('', 'mode index', 'mode index')
+        plotter.createSubPlot('', 'mode index from set 1', 'mode index from set 2')
         ax = plotter.figure.gca()
-        plotter.plotMatrix(ax, matrix, vmin, vmax, origin='lower')
+        plot = plotter.plotMatrix(ax, matrix, vmin, vmax, origin='lower')
+
+        x_ticklabels = np.arange(1, len(self.modes1)+1)
+        ax.xaxis.set_major_formatter(IndexFormatter(x_ticklabels))
+
+        y_ticklabels = np.arange(1, len(self.modes2)+1) 
+        ax.yaxis.set_major_formatter(IndexFormatter(y_ticklabels))
+
+        plotter.getColorBar(plot)
+        
         return [plotter]
 
     def _viewSingleMode(self, paramName):
         """ visualization for a selected mode. """
         matrix = prody.parseArray(self.protocol.matrixFile.getFileName())
 
-        if matrix.ndim == 2:
-            modes =  self.protocol.modes1.get()
+        if paramName == 'displayBarsSet1':
+            modes =  self.modes1
             modes_path = os.path.dirname(os.path.dirname(modes[1].getModeFile()))
 
-            modeNumber = self.modeNumber.get()
+            modeNumber = self.modeNumSet1.get()
             mode = modes[modeNumber]
             
             if modeNumber < 7 or mode is None:
@@ -120,9 +156,37 @@ class ProDyComparisonsViewer(ProtocolViewer):
                                         "the availables ones." % modeNumber,
                                         title="Invalid input")]
 
-            row = matrix[modeNumber]
+            row = matrix[modeNumber-1]
+            cumulOverlap = self.cumulOverlapSet1.get()
+
+            x_ticklabels = np.arange(1, len(modes)+1)
+
+        elif paramName == 'displayBarsSet2':
+            modes =  self.modes2
+            modes_path = os.path.dirname(os.path.dirname(modes[1].getModeFile()))
+
+            modeNumber = self.modeNumSet2.get()
+            mode = modes[modeNumber]
+            
+            if modeNumber < 7 or mode is None:
+                return [self.errorMessage("Invalid mode number *%d*\n"
+                                        "Display the output Normal Modes to see "
+                                        "the availables ones." % modeNumber,
+                                        title="Invalid input")]
+
+            row = matrix[:, modeNumber-1]
+            cumulOverlap = self.cumulOverlapSet2.get()
+
+            x_ticklabels = np.arange(1, len(self.modes2)+1) 
+
         else:
+            modes_sets = [self.modes1, self.modes2]
+            modes = modes_sets[np.argmax([len(nmset) for nmset in modes_sets])]
+
             row = matrix
+            cumulOverlap = self.cumulOverlapMain.get()
+
+            x_ticklabels = np.arange(1, len(modes)+1) 
 
         inds = np.arange(len(row))
 
@@ -140,9 +204,12 @@ class ProDyComparisonsViewer(ProtocolViewer):
             else:
                 ca.set_ylim([-1, 1])
 
-        if self.displayCumulOverlap:
+        if cumulOverlap:
             cum_overlaps = np.sqrt(np.power(row, 2).cumsum(axis=row.ndim-1))
             plotter.plotData(inds, cum_overlaps, 'r')
+
+        ax = plotter.figure.gca()
+        ax.xaxis.set_major_formatter(IndexFormatter(x_ticklabels))
             
         return [plotter]
 
