af7b7dffca9c3e1934f944272ce1a092e5798da2
James Krieger <jamesmkrieger@gmail.com>
2022-02-24 15:19:13 +0000

    add edit to mode viewer

--- prody2/viewers/viewer_modes.py
+++ prody2/viewers/viewer_modes.py
@@ -35,7 +35,7 @@ from pyworkflow.utils import *
 from pwem.objects import SetOfNormalModes
 from pwem.viewers import VmdView
 
-from prody2.protocols import ProDyANM, ProDyDefvec
+from prody2.protocols import ProDyANM, ProDyDefvec, ProDyEdit
 
 import os
 import prody
@@ -45,7 +45,7 @@ class ProDyModeViewer(Viewer):
         Normally, modes with high collectivity and low NMA score are preferred.
     """    
     _label = 'ProDy mode viewer'
-    _targets = [SetOfNormalModes, ProDyANM, ProDyDefvec]
+    _targets = [SetOfNormalModes, ProDyANM, ProDyDefvec, ProDyEdit]
     _environments = [DESKTOP_TKINTER, WEB_DJANGO]
 
     def _visualize(self, obj, **kwargs):
