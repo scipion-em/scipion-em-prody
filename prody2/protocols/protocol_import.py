91a4639b8d64f08344d5d0115d6837c7e57ba72c
James Krieger <jamesmkrieger@gmail.com>
2022-03-20 03:51:25 +0000

    import ens

--- prody2/protocols/protocol_import.py
+++ prody2/protocols/protocol_import.py
@@ -33,7 +33,7 @@ import os
 import numpy as np
 
 from pwem import *
-from pwem.objects import AtomStruct, SetOfNormalModes, String
+from pwem.objects import AtomStruct, SetOfNormalModes, String, EMFile
 from pwem.protocols import EMProtocol, ProtImportFiles
 
 from pyworkflow.utils import *
@@ -174,3 +174,127 @@ class ProDyImportModes(ProtImportFiles):
 
         self._defineOutputs(outputModes=nmSet)
         self._defineSourceRelation(self.inputStructure, nmSet)
+
+PDB = 0
+DCD = 1
+NPZ = 2
+
+NO_SUP = 0
+YES_SUP = 1
+ITERPOSE = 2
+
+class ProDyImportEnsemble(ProtImportFiles):
+    """
+    This protocol will import a ProDy Ensemble or PDBEnsemble
+    """
+    _label = 'Import ensemble'
+
+    # -------------------------- DEFINE param functions ----------------------
+    def _defineParams(self, form):
+        """ Define the input parameters that will be used.
+        Params:
+            form: this is the form to be populated with sections and params.
+        """
+
+        form.addSection(label='Import ensemble')
+
+        importChoices = self._getImportChoices()
+        filesCondition = self._getFilesCondition()
+
+        form.addParam('importFrom', params.EnumParam,
+                      choices=importChoices, default=self._getDefaultChoice(),
+                      label='Import from',
+                      help='Select the type of import.')
+
+        form.addParam('importType', params.EnumParam, choices=['pdb', 'dcd', 'npz'],
+                      default=PDB,
+                      label='Type of modes to import',
+                      help='ProDy can support import of modes in various file formats: \n'
+                           'native nmd and npz files, a scipion-style directory for a SetOfNormalModes, '
+                           'or a gromacs directory with ')
+
+        form.addParam('filesPath', params.PathParam,
+                      label="Files directory",
+                      help="Directory with the files you want to import.\n\n"
+                           "The path can also contain wildcards to select"
+                           "from several folders. \n\n"
+                           "Examples:\n"
+                           "  ~/project/data/day??_files/\n"
+                           "Each '?' represents one unknown character\n\n"
+                           "  ~/project/data/day*_files/\n"
+                           "'*' represents any number of unknown characters\n\n"
+                           "  ~/project/data/day##_files/\n"
+                           "'##' represents two digits that will be used as "
+                           "file ID\n\n"
+                           "NOTE: wildcard characters ('*', '?', '#') "
+                           "cannot appear in the actual path.)")
+
+        form.addParam('filesPattern', params.StringParam,
+                      label='Pattern',
+                      condition="importType!=%d" % SCIPION,
+                      help="Pattern of the files to be imported.\n\n"
+                           "The pattern can contain standard wildcards such as\n"
+                           "*, ?, etc, or special ones like ### to mark some\n"
+                           "digits in the filename as ID.\n\n"
+                           "NOTE: wildcards and special characters "
+                           "('*', '?', '#', ':', '%') cannot appear in the "
+                           "actual path.\n\n"
+                           "For gromacs modes, the first is for values and this is for vectors")
+
+        form.addParam('inputStructure', params.PointerParam, label="Input structure",
+                      pointerClass='AtomStruct', condition="importType!=%d" % PDB,
+                      help='The input structure can be an atomic model '
+                           '(true PDB) or a pseudoatomic model\n'
+                           '(an EM volume converted into pseudoatoms)')
+
+        form.addParam('superpose', params.EnumParam, label="Superpose?",
+                      choices=['No', 'Once', 'Iteratively'], default=NO_SUP,
+                      help='Elect whether and how to superpose the ensemble')        
+
+    # --------------------------- STEPS functions ------------------------------
+    def _insertAllSteps(self):
+        # Insert processing steps
+        self._insertFunctionStep('importModesStep')
+        self._insertFunctionStep('createOutputStep')
+
+    def importModesStep(self):
+        files_paths = self.getMatchFiles()
+        folder_path = os.path.split(files_paths[0])[0]
+        self.pattern1 = os.path.split(files_paths[0])[1]
+        
+        if self.importType == PDB:
+            if not self.pattern1.endswith('.pdb'):
+                self.pattern1 += '.pdb'
+            self.atoms = prody.parsePDB(os.path.join(folder_path, self.pattern1))
+            self.outEns = prody.PDBEnsemble(self.atoms)
+
+        elif self.importType == DCD:
+            if not self.pattern1.endswith('.dcd'):
+                self.pattern1 += '.dcd'
+            self.outEns = prody.parseDCD(os.path.join(folder_path, self.pattern1))
+
+        elif self.importType == NPZ:
+            if not self.pattern1.endswith('.ens.npz'):
+                self.pattern1 += '.ens.npz'
+            self.outEns = prody.loadEnsemble(os.path.join(folder_path, self.pattern1))
+
+        self.outEns.setAtoms(self.atoms)
+        if self.superpose == YES_SUP:
+            self.outEns.superpose()
+        elif self.superpose == ITERPOSE:
+            self.outEns.iterpose()
+        
+        self.filename = prody.saveEnsemble(self.outEns, self._getExtraPath('ensemble.ens.npz'))
+
+
+    def createOutputStep(self):
+        # fnSqlite = self._getPath('modes.sqlite')
+        # nmSet = SetOfNormalModes(filename=fnSqlite)
+        # nmSet._nmdFileName = String(self.nmdFileName)
+
+        # inputPdb = self.inputStructure.get()
+        # nmSet.setPdb(inputPdb)
+
+        outFile = EMFile(filename=self.filename)
+        self._defineOutputs(outputEns=self.outEns, outputFile=outFile)
+        #self._defineSourceRelation(self.inputStructure, nmSet)
