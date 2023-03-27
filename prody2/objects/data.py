99399d36f6822ca685518710ae3db6a60e3ffa3f
James Krieger <jamesmkrieger@gmail.com>
2023-03-27 11:51:34 +0200

    improved ensemble object

--- prody2/objects/data.py
+++ prody2/objects/data.py
@@ -5,12 +5,12 @@ class ProDyNpzEnsemble(SetOfTrajFrames):
     """Class for handling ProDy ens.npz files
     as sets of ensemble members"""
 
-    def writeNPZ(self, fnNPZ, orderBy='id', direction='ASC'):
+    def loadEnsemble(self, orderBy='id', direction='ASC'):
         """Make a new ensemble with all the items and write a new ens.npz file"""
         filenames = list(self.getFiles())
-        old_ensembles = [prody.loadEnsemble(filename) for filename in filenames]
+        old_ensembles = [prody.PDBEnsemble(prody.loadEnsemble(filename)) for filename in filenames]
 
-        new_ensemble = prody.Ensemble()
+        new_ensemble = prody.PDBEnsemble()
         if self.hasRef():
             if isinstance(self._ref.get(), TrajFrame):
                 ref = self._ref.get()
@@ -22,15 +22,16 @@ class ProDyNpzEnsemble(SetOfTrajFrames):
         for i, item in enumerate(self.iterItems(orderBy=orderBy,
                                                 direction=direction)):
             fname_index = filenames.index(item.getFileName())
-            conf_index = item.getIndex()
+            conf_index = item.getIndex() - 1 # back to python
 
             ensemble = old_ensembles[fname_index]
             coords = ensemble.getCoordsets(conf_index, selected=False)[0]
+            label = ensemble.getLabels()[conf_index]
+            weights = ensemble.getWeights()[conf_index]
             if not self.hasRef():
                 new_ensemble.setCoords(coords)
                 new_ensemble.setAtoms(ensemble.getAtoms())
                 self.setRef(item)
-            new_ensemble.addCoordset(coords)
+            new_ensemble.addCoordset(coords, weights, label)
 
-        #return new_ensemble
-        prody.saveEnsemble(new_ensemble, fnNPZ)
+        return new_ensemble
