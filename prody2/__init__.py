f13ed8bad23fee82a4ec01e52ae63450514288df
James Krieger <jamesmkrieger@gmail.com>
2024-06-04 18:34:41 +0200

    limit pyparsing

--- prody2/__init__.py
+++ prody2/__init__.py
@@ -86,7 +86,7 @@ class Plugin(pwem.Plugin):
         TEMPY_INSTALLED = 'tempy_installed'
         installTEMPy = installCmd.copy()
         installTEMPy.append('pip install biotempy==2.0.0 scikit-learn-extra '
-                            'threadpoolctl requests mdtraj && touch %s' % TEMPY_INSTALLED)
+                            'threadpoolctl requests mdtraj pyparsing==3.1.1 && touch %s' % TEMPY_INSTALLED)
         installCmd.pop(1) # remove conda create to only do it the first time
 
         # Install PDBFixer and OpenMM for ClustENM
