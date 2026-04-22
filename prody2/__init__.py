67a99bf8875158a421670a0c996ad2771735bd39
James Krieger <jamesmkrieger@gmail.com>
2026-04-22 16:24:46 +0100

    fix scipion env prody install

--- prody2/__init__.py
+++ prody2/__init__.py
@@ -108,7 +108,7 @@ class Plugin(pwem.Plugin):
 
         PRODY_SCIPION_INSTALLED = 'prody_scipion_installed'
         installCmd = [cls.getCondaActivationCmd(), f'conda activate {pwem.Config.getEnvName()} &&']
-        installCmd.append('pip uninstall prody -y && pip install -Ue . &&')
+        installCmd.append('pip uninstall prody -y && cd ProDy && pip install -Ue . && cd .. &&')
         installCmd.append('touch %s' % PRODY_SCIPION_INSTALLED)
         prodyCommands.append((" ".join(installCmd.copy()), PRODY_SCIPION_INSTALLED))
 
