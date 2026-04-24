c3f83b903748fa52e7f67a5ebb2252519fa8d74a
James Krieger <jamesmkrieger@gmail.com>
2026-04-24 12:49:54 +0100

    remove index formatter

--- prody2/viewers/viewer_compare.py
+++ prody2/viewers/viewer_compare.py
@@ -42,7 +42,6 @@ import numpy as np
 import os
 
 import prody
-# from prody.utilities.drawtools import IndexFormatter
 
 class ProDyComparisonsViewer(ProtocolViewer):
     """ Visualization of results from the ProDy mode comparison protocol.    
@@ -154,17 +153,7 @@ class ProDyComparisonsViewer(ProtocolViewer):
         plotter = EmPlotter()
         plotter.createSubPlot('', 'mode index from set 2', 'mode index from set 1')
         ax = plotter.figure.gca()
-        plot = prody.showMatrix(matrix, vmin=vmin, vmax=vmax, origin='lower')
-
-        y_ticklabels = np.arange(1, len(self.modes1)+1)
-
-        if os.path.isfile(self.protocol._getExtraPath('match_inds.txt')) and self.matchedModeNum.get():
-            x_ticklabels = prody.parseArray(self.protocol._getExtraPath('match_inds.txt'), dtype=int)
-        else:
-            x_ticklabels = np.arange(1, len(self.modes2)+1) 
-
-        # ax.xaxis.set_major_formatter(IndexFormatter(x_ticklabels))
-        # ax.yaxis.set_major_formatter(IndexFormatter(y_ticklabels))
+        prody.showMatrix(matrix, vmin=vmin, vmax=vmax, origin='lower')
 
         if self.allticks.get():
             ax.xaxis.set_major_locator(ticker.IndexLocator(offset=0.5, base=1.))
@@ -263,8 +252,6 @@ class ProDyComparisonsViewer(ProtocolViewer):
         if cumulOverlap:
             cum_overlaps = np.sqrt(np.power(row, 2).cumsum(axis=row.ndim-1))
             plotter.plotData(inds, cum_overlaps, 'r')
-        
-        # ax.xaxis.set_major_formatter(IndexFormatter(x_ticklabels))
 
         if self.allticks.get():
             ax.xaxis.set_major_locator(ticker.IndexLocator(offset=0.5, base=1.))
